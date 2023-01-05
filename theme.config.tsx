f6f7bd12db3f399169b99a855543edfec9450ba9
Milind Mishra <milind.mishra4@gmail.com>
2023-01-05 12:20:45 +0530

    🐛 FIX: changes

--- theme.config.tsx
+++ theme.config.tsx
@@ -3,13 +3,6 @@ import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
   logo: <span>Locus Docs</span>,
-  project: {
-    link: "https://github.com/locusconnect",
-  },
-  chat: {
-    link: "https://discord.com",
-  },
-  docsRepositoryBase: "https://github.com/locusconnect/locus-docs",
   footer: {
     text: "Locus Documentation",
   },
